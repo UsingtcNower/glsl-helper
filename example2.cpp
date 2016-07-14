c3d57679ed21b256b451322739d7e2adb0622db6
tao zhang <nowerzt@gmail.com>
2016-07-14 09:51:07 +0800

    update

--- example2.cpp
+++ example2.cpp
@@ -31,53 +31,6 @@ cv::Mat img;
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, img.cols, img.rows, 0, GL_RGB, GL_UNSIGNED_BYTE, img.data);
 
-	FILE* hFile = fopen("D:\\code\\glman\\shaders\\private\\1.dat", "rb");
-	int w = 256, h = 256;
-	char* buf  = new char[w*h];
-	fread(buf, 1, w*h, hFile);
-	fclose(hFile);
-	cv::Mat img1(w, h, CV_8UC4);
-	int k = 0;
-	for(int i=0;i<img1.rows;++i) {
-		uchar* p = img1.ptr<uchar>(i);
-		for(int j=0;j<img1.cols;++j) {
-			p[j+3] = buf[k++];
-			p[j] = p[j+1] = p[j+2] = 0;
-		}
-	}
-	delete[] buf;
-
-	glBindTexture(GL_TEXTURE_2D, texId[1]);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_ALPHA, img1.cols, img1.rows, 0, GL_ALPHA, GL_UNSIGNED_BYTE, img1.data);
-
-
-	hFile = fopen("D:\\code\\glman\\shaders\\private\\2.dat", "rb");
-	w = 2, h = 1024;
-	buf  = new char[w*h];
-	fread(buf, 1, w*h, hFile);
-	fclose(hFile);
-	cv::Mat img2(w, h, CV_8UC4);
-	k = 0;
-	for(int i=0;i<img2.rows;++i) {
-		uchar* p = img2.ptr<uchar>(i);
-		for(int j=0;j<img2.cols;++j) {
-			p[j+3] = buf[k++];
-			p[j] = p[j+1] = p[j+2] = 0;
-		}
-	}
-	delete[] buf;
-
-	glBindTexture(GL_TEXTURE_2D, texId[1]);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_ALPHA, img1.cols, img1.rows, 0, GL_ALPHA, GL_UNSIGNED_BYTE, img1.data);
-
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 	glRotatef(180, 0, 0, 1);
@@ -119,13 +72,16 @@ void glslProcess()
 	if(!glprog.Create("D:\\code\\glman\\shaders\\private\\mopi_meibai.vert", "D:\\code\\glman\\shaders\\private\\mopi_meibai.frag")) {
 		return ;
 	}
+	glprog.SetUniform("uMopiSigma", (float)1.0);
+	glprog.SetUniform("uDelta", (float)0.01);
+	glprog.SetUniform("uWhiteGain", (float)10);
 	glprog.Use();
 
 }
 
 void main(int argc, char ** argv)
 {
-	//glutInit(&argc, argv);          // initialize the toolkit
+	glutInit(&argc, argv);          // initialize the toolkit
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB); // set the display mode
 	glutInitWindowSize(screenWidth, screenHeight); // set the window size
 	glutInitWindowPosition(0, 0); // set the window position on screen
